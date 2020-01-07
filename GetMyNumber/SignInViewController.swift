0582f6fa69606ccf25b60d3da8d91e15c344e0c8
dinaolmelak <dinaolmelak@gmail.com>
2020-01-07 16:32:57 -0600

    Set up Sign up

--- GetMyNumber/SignInViewController.swift
+++ GetMyNumber/SignInViewController.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import Parse
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onTapSignUp(_ sender: Any) {
+        if usernameField.text == nil || usernameField.text == "" || passwordField.text == nil || passwordField.text == ""{
+            showAlert(title: "Invalid Entry", message: "Please check username or password")
+            
+        } else{
+            
+            var user = PFUser()
+            user.username = usernameField!.text
+            user.password = passwordField!.text
+            // other fields can be set just like with PFObject
+            
+            user.signUpInBackground { (success, error) in
+                if error != nil{
+                    print(error!)
+                    
+                } else{
+                    self.performSegue(withIdentifier: "SignedIn", sender: self)
+                    
+                }
+            }
 
+        }
+        
+    }
+    @IBAction func onTapSignIn(_ sender: Any) {
+        
+        
+    }
+
+    func showAlert(title: String, message: String){
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
