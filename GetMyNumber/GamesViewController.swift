1019eaa2f841f83e6a4835f967ff47290091051f
dinaolmelak <dinaolmelak@gmail.com>
2020-01-08 19:24:28 -0600

    Successfully implemented Saving and Query

--- GetMyNumber/GamesViewController.swift
+++ GetMyNumber/GamesViewController.swift
@@ -7,16 +7,71 @@
 //
 
 import UIKit
+import Parse
 
-class GamesViewController: UIViewController {
-
+class GamesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    var onlinePlayers = [PFObject]()
+    @IBOutlet weak var playersTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        playersTableView.delegate = self
+        playersTableView.dataSource = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        let game = PFObject(className: "Game")
+        game["user"] = PFUser.current()
+        game["isOnline"] = true
+        game.saveInBackground { (succes, error) in
+            if error != nil {
+                print(error!)
+            } else{
+                print("saved status online")
+            }
+        }
+        getPlayers()
     }
     
+    func getPlayers(){
+        let query = PFQuery(className: "Game")
+        query.whereKey("isOnline", equalTo: true)
+        query.includeKey("user")
+        query.findObjectsInBackground { (gamesArray, error) in
+            if error != nil{
+                print(error!)
+            } else{
+                self.onlinePlayers = gamesArray!
+                print(gamesArray)
+                self.playersTableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return onlinePlayers.count + 1
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "PlayersCell")
+            cell.textLabel!.text = PFUser.current()!.username
+            cell.detailTextLabel!.text = "Online"
+            return cell
+        } else{
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: "PlayersCell")
+            let game = onlinePlayers[indexPath.row - 1]
+            let user = game["user"] as! PFUser
+            let isOnline = game["isOnline"] as! Bool
+            cell.textLabel!.text = user.username
+            cell.detailTextLabel!.text = String(isOnline)
+            
+            
+            return cell
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #  Get My Number
-### This is a fun little game, it is currently being built
+### This is a fun little game, it is currently being built. Please feel free to raise any Issues or concerns after observation.
 
